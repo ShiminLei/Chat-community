c46cca71b9e14ed25d982cc113ad8e3305780af0
ShiminLei <sl2kd@virginia.edu>
2020-05-17 11:23:14 -0400

    exception

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -44,7 +44,12 @@ public class HomeController {
             }
         }
         model.addAttribute("discussPosts", discussPosts);
-        return "/index"; // index.html
+        return "/index";
+    }
+
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500";
     }
 
 }
