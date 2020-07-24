a7f9b94c26d0d0bc5617824b9da892c8e56f3b25
ShiminLei <sl2kd@virginia.edu>
2020-07-24 16:39:33 -0400

    final

--- src/main/java/com/nowcoder/community/controller/advice/ExceptionAdvice.java
+++ src/main/java/com/nowcoder/community/controller/advice/ExceptionAdvice.java
@@ -25,7 +25,7 @@ public class ExceptionAdvice {
         }
 
         String xRequestedWith = request.getHeader("x-requested-with");
-        if ("XMLHttpRequest".equals(xRequestedWith)) {
+        if ("XMLHttpRequest".equals(xRequestedWith)) { // 说明是异步请求
             response.setContentType("application/plain;charset=utf-8");
             PrintWriter writer = response.getWriter();
             writer.write(CommunityUtil.getJSONString(1, "服务器异常!"));
